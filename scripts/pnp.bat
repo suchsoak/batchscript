10e579273d5318cb555fd490bdcf1224e4eae376
~#M?x <89079595+suchsoak@users.noreply.github.com>
2025-08-15 19:23:50 -0300

    Update pnp.bat

--- scripts/pnp.bat
+++ scripts/pnp.bat
@@ -1,35 +1,55 @@
+cls
+color 1
+@echo ============================================================
+@echo =                RESETAR DRIVER DE VÍDEO                   =
+@echo ============================================================
+@echo.
+@echo [*] Github: https://github.com/suchsoak
+@echo ------------------------------------------------------------
+@echo [!] Listando dispositivos de vídeo...
+@echo ------------------------------------------------------------
 pnputil /enum-devices /class Display | findstr "ID da Instância"
 @echo.
+set /p ID=Digite a ID da Instância do driver de vídeo: 
 @echo.
-set/p ID= Coloque a ID da Instancia:
-@echo.
-@echo.
+@echo [*] Reiniciando dispositivo de vídeo...
 pnputil /restart-device "%ID%"
-
 @echo.
-set /p reiniciar= escolha uma opcao:
+color 7
+@echo ------------------------------------------------------------
+@echo Deseja reiniciar o computador agora?
+@echo ------------------------------------------------------------
+@echo [s] Sim
+@echo [n] Não
+@echo ------------------------------------------------------------
+set /p reiniciar=Escolha uma opção [S/n]: 
 @echo.
-if %reiniciar% equ s goto reiniciars 
-if %reiniciar% equ n goto reiniciarn
 
-if %reiniciar% equ nul (
-  @echo.
-  @echo Escolha uma opcao!
-  @echo.
-  @pause
-) else (
-  color 2
-  @echo.
-  @echo Opcao invalida!!
-  @echo.
-  @pause
-  exit
-)
+if /i "%reiniciar%"=="s" goto reiniciars
+if /i "%reiniciar%"=="S" goto reiniciars
+if /i "%reiniciar%"=="y" goto reiniciarn
+if /i "%reiniciar%"=="Y" goto reiniciarn
+if /i "%reiniciar%"=="n" goto reiniciarn
+if /i "%reiniciar%"=="N" goto reiniciarn
+
+color 4
+@echo.
+@echo [!] Opção inválida! Por favor, escolha 's' ou 'n'.
+@echo.
+@pause
+exit
 
 :reiniciars
-
+color 2
+@echo.
+@echo [*] Reiniciando o computador...
 shutdown /r /c "O chefe mandou, vá descansar"
+exit
 
 :reiniciarn
-
+color 2
+@echo.
+@echo [*] Processo finalizado sem reiniciar.
+@echo.
+@pause
 exit
