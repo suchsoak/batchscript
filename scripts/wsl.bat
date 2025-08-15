3aab37d5495fb2e9e6adbdfad513f63f30a3fbb0
~#M?x <89079595+suchsoak@users.noreply.github.com>
2025-08-15 19:20:05 -0300

    Create wsl.bat

---
+++ scripts/wsl.bat
@@ -0,0 +1,22 @@
+cls
+@echo.
+@echo.
+@echo -----------------------------------
+@echo github: https://github.com/Suchsoak
+@echo -----------------------------------
+@echo.
+@echo Baixando e instalando o WSL (Windows Subsystem for Linux)
+@echo.
+@echo wsl --install
+@echo.
+wsl --install
+@echo.
+@echo [!] O WSL foi instalado com sucesso.
+@echo.
+@echo. Para instalar uma distribuicao Linux, execute o comando:
+@echo wsl --install -d <nome_da_distribuicao>
+@echo.
+@echo Exemplo: wsl --install -d Ubuntu
+@echo.
+@echo [!] Reinicie o computador para concluir a instalacao.
+@pause
